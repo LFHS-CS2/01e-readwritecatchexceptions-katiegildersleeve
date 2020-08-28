e4475b17d22f8b8c31ed1a4f02ee5ad341344d31
Katelyn Gildersleeve <2270245@lfschools.net>
2020-08-28 19:00:42 +0000

    exceptions

--- Main.java
+++ Main.java
@@ -2,13 +2,25 @@ import java.io.*;
 import java.util.*;
 class Main {
     public static void main(String[] args)  {
+      try{
        Scanner in = new Scanner(new File("test.in"));
        PrintWriter out = new PrintWriter("test.out");
-       while (in.hasNextLine()) {      
-           out.println(in.nextLine());   
-       }   
+        while (in.hasNextLine()) {      
+             out.println(in.nextLine()); 
+           } 
         in.close();   
         out.close();
+      } 
+           catch(FileNotFoundException I){
+             System.err.println("File not Found");
+           }
+           catch(IOException IOE){
+             System.err.println("Input Error");
+           }   
+           catch(Exception E){
+             System.err.println("General Exception");
+           }
         
+         
+        }
     }
-}
